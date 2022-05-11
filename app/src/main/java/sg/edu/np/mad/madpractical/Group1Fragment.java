fa9bf3ffccd3d706aceb9c3a025ff7bcc91bb764
Shuqri Shaifuddin <s10227932@connect.np.edu.sg>
2022-05-11 11:15:50 +0800

    Prac 3

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/Group1Fragment.java
@@ -0,0 +1,20 @@
+package sg.edu.np.mad.madpractical;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Group1Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_group1, container, false);
+        return view;
+    }
+}
